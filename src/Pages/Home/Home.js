545dfde15781c6e9ebe52a520d7f3fe82f26b52d
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2023-01-14 04:26:16 +0600

    error loading data

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Information from './Id/Information';
+import female from '../../assets/img/Female01.jpg'
+import Event from './Event/Event';
 
 const Home = () => {
     return (
-        <div>
-            <h3>Content</h3>
+        <div className='flex ml-5'>
+            <div> <Information></Information> </div>
+            <div className='ml-84 mt-12'>
+                <p className='text-center text-lg font-bold'>Female</p>
+                <img className='w-3/4 ml-40' src={female} alt="" />
+            </div>
+            <div className=''> <Event></Event> </div>
         </div>
     );
 };
---
+++ src/Pages/Home/Id/Information.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const Information = () => {
+    return (
+        <div className='mt-20'>
+            <p className='font-bold text-2xl'>EVT0050</p>
+            <p className='font-bold text-xl'>Person Detected</p>
+            <br />
+            <p className=''>Name:</p>
+            <p className=''>Location:</p>
+            <p className=''>Date:</p>
+            <p className=''>Time:</p>
+            <br />
+            <p className=''>Description:</p>
+
+        </div>
+    );
+};
+
+export default Information;
